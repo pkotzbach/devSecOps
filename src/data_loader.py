9a2a61a5fbc464df95b110c04118ec9767f8b6aa
pkotzbach <pawkotz@gmail.com>
2025-02-08 13:18:28 +0100

    add percentage

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,16 +3,20 @@ import numpy as np
 import config
 import tinygrad
 
-def load_data(path:str):
+def load_data(path:str, batch_size:int, percentage:float=1.0):
+        print("Loading data from", path)
         df = pd.read_csv(path)
         df = df[df["processed_func"].str.len() < 500] # remove long functions
         # predict CWEs
         df = df[((df["CWE ID"].isin(config.CWEs)) & (df["target"] == 1)) | (df["target"] == 0)]
         df["CWE target"] = np.where(df["target"] == 1, df["CWE ID"], np.nan)
         # downsampling nan
-        df = pd.concat([df[df["CWE target"].isna() == False], df[df["CWE target"].isna() == True].sample(10000)])
+        df = pd.concat([df[df["CWE target"].isna() == False], df[df["CWE target"].isna() == True].sample(len(df[df["CWE target"].isna() == False])*3)])
 
         X = df["processed_func"].tolist()
         y = df["CWE target"].tolist()
         y = tinygrad.Tensor([config.CWEs.index(i) if not pd.isna(i) else 0 for i in y], requires_grad=False)
+        X = X[:int(len(X)*percentage)//batch_size*batch_size]
+        y = y[:int(len(y)*percentage)//batch_size*batch_size]
+        print("Data loaded: X", len(X), "y", len(y))
         return X, y
--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ def test(args, model, test_data):
         loss, f1 = model.test(batch_X, batch_y)
         losses.append(loss.mean().numpy())
         f1s.append(f1.mean())
+        # print(f1s[-1])
 
     return np.sum(losses)/len(losses), np.sum(f1s)/len(f1s)
 
@@ -26,12 +27,13 @@ def train(args, model, train_data, eval_data):
     for epoch in range(epochs):
         losses = []
         for i in range(0, len(X_train), batch_size):
+            # print(f"batch {i//batch_size}/{len(X_train)//batch_size}")
             batch_X = X_train[i:i + batch_size]
             batch_y = y_train[i:i + batch_size]
             loss = model.train(batch_X, batch_y)
             losses.append(loss.mean().numpy())
 
-        if epoch % 5 == 0:
+        if epoch % 10 == 0:
             eval_loss, eval_f1 = test(args, model, eval_data)
             print(f"Epoch {epoch}: loss={np.sum(losses)/len(losses)}, eval_loss={eval_loss}, eval_f1={eval_f1}")
         else:
@@ -44,27 +46,39 @@ def main():
                         help="vLLM model for embeddings.")
     parser.add_argument("--dropout", default=0.1, type=float, required=False,
                         help="Dropout ratio.")
-    parser.add_argument("--epochs", default=10, type=int, required=False,
+    parser.add_argument("--epochs", default=100, type=int, required=False,
                         help="Epochs.")
     parser.add_argument("--batch_size", default=10, type=int, required=False,
                         help="Batch size.")
     parser.add_argument("--lr", default=0.01, type=float, required=False,
                         help="Learning rate.")
-    parser.add_argument("--do_train", default=True, type=bool, required=False,
+    parser.add_argument("--train", default=1, type=int, required=False,
                         help="Do train.")
+    parser.add_argument("--test", default=1, type=int, required=False,
+                        help="Do test.")
     parser.add_argument("--layers", default=[768, 128, 64, 19], type=list, required=False,
                         help="Sizes of hidden neurons (has to start with 768 and end with 19).")
+
     parser.add_argument("--data_folder", default="~/devSecOps/data/", type=str, required=False,
                         help="Path to folder with data.")
+    parser.add_argument("--percentage", default=1, type=float, required=False,
+                        help="Percentage of datasets (1 = 100%).")
     
     args = parser.parse_args()
 
     llm = LLM(model=args.model, task="embed")
     myModel = MyModel(llm, args)
-    if args.do_train:
-        train_data = load_data(args.data_folder + "train.csv")
-        eval_data = load_data(args.data_folder + "eval.csv")
+    if args.train:
+        print("=== TRAIN ===")
+        train_data = load_data(args.data_folder + "train.csv", args.batch_size, args.percentage)
+        eval_data = load_data(args.data_folder + "eval.csv", args.batch_size, args.percentage)
         train(args, myModel, train_data, eval_data)
+
+    if args.test:
+        print("=== TEST ===")
+        test_data = load_data(args.data_folder + "test.csv", args.batch_size, args.percentage)
+        test_loss, test_f1 = test(args, myModel, test_data)
+        print(f"Test loss={test_loss}, Test f1={test_f1}")
 
 if __name__ == "__main__":
     main()
